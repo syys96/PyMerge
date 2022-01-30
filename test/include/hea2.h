6ce5379447efc8f46eabcc0a27740f4c6a0415d4
syys96 <3359652182@qq.com>
2022-01-30 10:52:10 +0800

    test third_lib merge

---
+++ test/include/hea2.h
@@ -0,0 +1,10 @@
+//
+// Created by syys on 2022/1/30.
+//
+
+#ifndef TEST_MERGE_HEA2_H
+#define TEST_MERGE_HEA2_H
+
+
+
+#endif //TEST_MERGE_HEA2_H
---
+++ test/third_lib/include/third_lib.h
@@ -0,0 +1,6 @@
+#ifndef _THIRD_LIB_INCLUDED_
+#define _THIRD_LIB_INCLUDED_ 
+
+void info_print();
+
+#endif 
